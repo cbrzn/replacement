38bff8b2385b6bdd5fe5c62c4d69b2502b8b3467
cbrzn <cesarbrazon10@gmail.com>
2018-02-27 22:56:49 -0400

    hablale

--- controllers/order.js
+++ controllers/order.js
@@ -122,7 +122,7 @@ router.post('/deliver_done', (req, res) => {
     var a = payment_day - 28;
     month += 1;
     payment_day = a;
-  } else if ((month % 2 === 0 && month < 8) || month === 7 || (!(month % 2 === 0) && month > 7)) {
+  } else if ((month % 2 === 0 && month < 8 && payment_day >= 30) || month === 7 || (!(month % 2 === 0) && month > 7 && payment_day >= 30)) {
     var a = payment_dat = 30;
     payment_day = a;
     month += 1;
