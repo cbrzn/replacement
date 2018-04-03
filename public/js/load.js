ad5845e0b30740be693e47573f6aed4336ce9201
brosej <bjunior7@hotmail.com>
2018-04-03 15:31:54 -0400

    Bug fix: upload page reload

--- public/js/load.js
+++ public/js/load.js
@@ -1,6 +1,17 @@
 function $(id) {
     return document.getElementById(id);
 };
+
+const iconifyEraseButton = (mybutton)=>{
+    var erase_icon = document.createElement('i');
+    mybutton.setAttribute('class','btn btn-danger p-1 ml-2')
+    erase_icon.setAttribute('class','ion-icon ion-android-delete p-1')
+    erase_icon.setAttribute('style','font-size:22px;color:white')
+    //mybutton.innerHTML = ""
+    mybutton.appendChild(erase_icon)
+    return mybutton
+  }
+
 function sendFile(){
   let xhr = new XHR();
     let formData = new FormData();
@@ -21,48 +32,90 @@ function sendFile(){
      xhr.post(`./product/create`,{price:precio, description:description, stock:stock, type_supplier:type_supplier, brand:brand, department:department, code:code},{'Content-Type':'application/json'}).then((data)=>{
        if (data.status === 200) {
           alert("Producto cargado");
-          window.location.href = './upload.html';
         } else {
           alert("Error al cargar el producto")
         }
-  });
+  }).then( () =>{ $('close_upload_modal').click() } );
 };
 
 function select() {
   let xhr = new XHR();
-  $('add_stuff').style.margin = "0 auto";
-  $('add_stuff').style.display = "block";
-  $('show_stuff').style.margin = "0 auto";
-  $('show_stuff').style.display = "block";
+  //$('add_stuff').style.margin = "0 auto";
+  //$('add_stuff').style.display = "block";
+  //$('show_stuff').style.margin = "0 auto";
+  //$('show_stuff').style.display = "block";
   xhr.get('./stuff/get_brands_and_departments',{},{}).then((data)=> {
       for (var i=0; i<data.brands.length; i++) {
-        var brand = document.createElement("option");
-        brand.value = data.brands[i].name;
-        brand.innerHTML = data.brands[i].name;
+        var brand_option = document.createElement("option");
+        brand_option.value = data.brands[i].name;
+        brand_option.innerHTML = data.brands[i].name;
         if (data.brands[i].name !== null) {
-          $('brand').appendChild(brand);
+          $('brand_select').appendChild(brand_option);
         }
       }
       for (var i=0; i<data.departments.length; i++) {
-          var department = document.createElement("option");
-          department.value = data.departments[i].name;
-          department.innerHTML = data.departments[i].name;
+          var department_option = document.createElement("option");
+          department_option.value = data.departments[i].name;
+          department_option.innerHTML = data.departments[i].name;
           if (data.departments[i].name !== null) {
-            $('department').appendChild(department);
+            $('department_select').appendChild(department_option);
           }
       }
 
-      $('add_stuff').addEventListener('click', function() {
-          $('upload_form').innerHTML = "";
-          $('stuff').style.display = "block";
-          $('add_stuff').style.display = "none";
-          var new_button = document.createElement('button');
-          new_button.innerHTML = "anadir";
-          new_button.setAttribute('id', 'new_add');
-          $('add_btn').appendChild(new_button)
-            $('new_add').addEventListener('click', function() {
-              var marca = $('brand').value;
-              var departamento = $('department').value;
+      //CREAR MODAL DE AGREGAR NUEVAS MARCAS.
+
+      $('add_stuff_btn').addEventListener('click', function() {
+          //fuck all this
+          
+          //till here
+            //  $('message_modal_button').setAttribute('id', 'new_add');
+          //esta linea de abajo no va 
+              
+              
+          //esto si
+              
+              var add_stuff_div= document.createElement('div')
+              add_stuff_div.setAttribute('class','col')
+              var firstspan = document.createElement('span')
+              firstspan.innerHTML = 'Nueva marca'
+              firstspan.setAttribute('class','pb-2')
+              var secondspan = document.createElement('span')
+              secondspan.innerHTML = 'Nuevo departamento'
+              var new_brand = document.createElement('input')
+              new_brand.setAttribute('class','pb-2')
+              new_brand.setAttribute('id','new_brand_input')
+              new_brand.setAttribute('class','form-control')
+              new_brand.setAttribute('type','text')
+              new_brand.setAttribute('placeholder','marca')
+              var new_dept = document.createElement('input')
+              new_dept.setAttribute('id','new_dept_input')
+              new_dept.setAttribute('class','form-control')
+              new_dept.setAttribute('type','text')
+              new_dept.setAttribute('placeholder','departamento')
+              $('message_modal_title').innerHTML = "Agregar nueva marca y/o departamento"
+              $('message_modal_button').innerHTML = "Agregar"
+              $('message_modal_body').appendChild(firstspan)
+              $('message_modal_body').appendChild(new_brand)
+              $('message_modal_body').appendChild(secondspan)
+              $('message_modal_body').appendChild(new_dept)
+
+
+              
+              
+
+              $('close_upload_modal').click()
+              $('trigger_message_modal').click()
+              
+
+              $('message_modal_button').addEventListener('click', () => {
+
+                //var new_brand_input = $(new_brand_input)
+                //var new_dept_input = $(new_dept_input)
+                var marca = new_brand.value;
+                var departamento = new_dept.value;
+                console.log(marca)
+                console.log(departamento)
+
               if (marca !== "" && departamento === "") {
                 xhr.post('./stuff/add_brand',{brand:marca},{'Content-Type':'application/json'}).then((data) => {
                    if (data.status === 200) {
@@ -90,19 +143,20 @@ function select() {
                  });
               });
              }
-           });
+              
+            })
+
          });
 
-        $('show_stuff').addEventListener('click', function() {
+        $('show_stuff_btn').addEventListener('click', function() {
           $('tr').innerHTML = "";
           if ($('tbdy') != null) {
             $('tbdy').remove();
           }
-          $('upload_form').innerHTML = "";
-          $('add_stuff').style.display = "none";
-          $('show_stuff').style.display = "none";
+          //las 3 lineas de abajo no van
           xhr.get('./stuff/get_brands_and_departments',{},{}).then((data) => {
                         var tbl = $('table');
+                        tbl.setAttribute('class','table table-striped table-hover table-responsive-sm table-bordered')
                         var tbdy = document.createElement('tbody');
                         tbdy.setAttribute('id', 'tbdy')
                         for (var i=0; i < 2; i++) {
@@ -110,6 +164,7 @@ function select() {
                             switch(i) {
                               case 0:
                                 th.innerHTML = 'Marca';
+                                
                               break;
                               case 1:
                                 th.innerHTML = 'Departamento';
@@ -120,22 +175,35 @@ function select() {
 
                             var size = data.brands.length > data.departments.length ? data.brands.length : data.departments.length;
 
+                            buttons =[]
                             for (var i = 0; i < size; i++) {
                                 var tr = document.createElement('tr');
+                                
                                 tr.setAttribute('id', i);
                                 for (var j = 0; j < 2; j++) {
+
                                   var td = document.createElement('td');
+                                  buttons[i] = document.createElement('button')
+                                  buttons[i] = iconifyEraseButton(buttons[i])
                                   switch (j) {
                                     case 0:
                                       if (data.brands[i] !== undefined) {
+
                                         td.setAttribute("id", data.brands[i].name)
+                                        buttons[i].setAttribute('id',data.brands[i].name)
+                                        td.appendChild(buttons[i])
+
                                       }
                                       if (data.brands[i] === undefined) {
+
                                         td.innerHTML = "";
+
                                       } else {
+
                                         var name = data.brands[i].name;
                                         td.innerHTML = name;
-                                        td.addEventListener('click', function(){
+                                        td.appendChild(buttons[i])
+                                        buttons[i].addEventListener('click', function(){
                                           var r = confirm("Segur@ que desea eliminar la marca: "+this.id)
                                           if (r == true) {
                                             xhr.get(`./stuff/delete_brand/${this.id}`,{},{}).then((data) => {
@@ -143,17 +211,20 @@ function select() {
                                             })
                                           }
                                         });
+
                                       }
                                     break;
                                     case 1:
                                       if (data.departments[i] !== undefined) {
+                                        buttons[i].setAttribute('id', data.departments[i].name )
                                         td.setAttribute("id", data.departments[i].name)
                                       }
                                       if (data.departments[i] === undefined) {
                                         td.innerHTML = "";
                                       } else {
                                         td.innerHTML = data.departments[i].name;
-                                        td.addEventListener('click', function(){
+                                        td.appendChild(buttons[i])
+                                        buttons[i].addEventListener('click', function(){
                                           var r = confirm("Segur@ que desea eliminar el departamento: "+this.id)
                                           if (r == true) {
                                             xhr.get(`./stuff/delete_department/${this.id}`,{},{}).then((data) => {
@@ -170,8 +241,15 @@ function select() {
                                 tbdy.appendChild(tr);
                             }
                         tbl.appendChild(tbdy);
+                        $('message_modal_title').innerHTML= "Marcas y departamentos"
+                        $('message_modal_body').innerHTML= ""
+                        $('message_modal_button').setAttribute('style','display:none;')
+                        $('message_modal_body').appendChild(tbl)
+          }).then( () => { 
+            $('close_upload_modal').click()
+            
 
-          });
+          } ).then(()=> { $('trigger_message_modal').click() })
         });
       });
      };
